beb383596992b94b4fb7376e9e335238926f62a5
dinshor <121557619+dinshor@users.noreply.github.com>
2025-03-08 17:59:30 +0200

    Update client.py

--- client/client.py
+++ client/client.py
@@ -1,9 +1,17 @@
+import os
 import time
 import requests
 import urllib3
+
 # Suppress InsecureRequestWarning
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Ensure the key log file is set correctly
+os.environ["SSLKEYLOGFILE"] = "/tmp/sslkey.log"
+print(f"SSL key log file set to: {os.environ.get('SSLKEYLOGFILE')}")
+
 SERVER_URL = "https://server:8443"
+
 while True:
     try:
         print("Sending request to server...")
